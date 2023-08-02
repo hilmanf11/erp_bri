41d8cfa08220f9921c9467ad9394643eeb3d8ba2
kurniawan <kurniawan130795@gmail.com>
2023-08-02 15:57:54 +0700

    Working Calendar & Engineering/Process

---
+++ application/controllers/master/Working_calendar.php
@@ -0,0 +1,216 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Working_calendar extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        $this->load->model('crud');
+        //$this->load->model('banshu');
+        //$this->pg = $this->load->database('pg', TRUE);
+
+        //Validasi Form
+        $this->form_validation->set_rules('product_no', 'Product No', 'required|min_length[2]|max_length[50]|is_unique[generate_mps.product_no]');
+    }
+
+    public function index()
+    {
+        if (empty($this->session->username)) {
+            redirect('error_session');
+        } elseif ($this->checkuserAccess($this->id_menu()) > 0) {
+            $data['button'] = $this->getbutton($this->id_menu());
+            $this->load->view('template/header', $data);
+            $this->load->view('master/working_calendar');
+        } else {
+            redirect('error_access');
+        }
+    }
+
+    public function hkw()
+    {
+        $bulan = $this->input->post('month');
+        $tahun = $this->input->post('year');
+
+        if ($bulan == "" or $tahun == "") {
+            $bulan = date('m');
+            $tahun = date('Y');
+        }
+
+        $hari = "01";
+        $jumlahhari = date("t", mktime(0, 0, 0, $bulan, $hari, $tahun));
+        $s = date("w", mktime(0, 0, 0, $bulan, 1, $tahun));
+
+        $hkw = 0;
+        for ($d = 1; $d <= $jumlahhari; $d++) {
+            $tanggal = $tahun . "-" . $bulan . "-" . $d;
+            $this->db->select('remarks');
+            $this->db->from('working_calendar');
+            $this->db->where('deleted', 0);
+            $this->db->where('working_date', $tanggal);
+            $data = $this->db->get()->result_array();
+
+            $hkw += 1;
+
+            if (@$data[0]['remarks'] != "") {
+                $hkw -= 1;
+            }
+
+            if (date("l", mktime(0, 0, 0, $bulan, $d, $tahun)) == "Sunday") {
+                $hkw -= 1;
+            }
+        }
+
+        echo $hkw;
+    }
+
+    public function calendars()
+    {
+        $bulan = $this->input->post('month');
+        $tahun = $this->input->post('year');
+
+        if ($bulan == "" or $tahun == "") {
+            $bulan = date('m');
+            $tahun = date('Y');
+        }
+
+
+        $hari = "01";
+        $jumlahhari = date("t", mktime(0, 0, 0, $bulan, $hari, $tahun));
+
+        $html = '<style>
+                    body {
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+
+                    #customers {
+                        border-collapse: collapse;
+                        width: 100%;
+                        font-size: 10px;
+                    }
+
+                    #customers td,
+                    #customers th {
+                        border: 1px solid #ddd;
+                        padding: 2px;
+                        height:50px;
+                    }
+
+                    #customers tr:nth-child(even) {
+                        background-color: #f2f2f2;
+                    }
+
+                    #customers tr:hover {
+                        background-color: #ddd;
+                    }
+
+                    #customers th {
+                        padding-top: 2px;
+                        padding-bottom: 2px;
+                        text-align: left;
+                        color: black;
+                    }
+                </style>
+                <table id="customers" style="width: 100%;">
+                    <tr>
+                        <td align=center width="200">
+                            <font color="#FF0000">Sunday</font>
+                        </td>
+                        <td align=center width="200">Monday</td>
+                        <td align=center width="200">Tuesday</td>
+                        <td align=center width="180">Wednesday</td>
+                        <td align=center width="200">Thursday</td>
+                        <td align=center width="200">Friday</td>
+                        <td align=center width="200">Saturday</td>
+                    </tr>';
+        $s = date("w", mktime(0, 0, 0, $bulan, 1, $tahun));
+
+        for ($ds = 1; $ds <= $s; $ds++) {
+            $html .= "<td></td>";
+        }
+
+        for ($d = 1; $d <= $jumlahhari; $d++) {
+            if (date("w", mktime(0, 0, 0, $bulan, $d, $tahun)) == 0) {
+                $html .= "<tr>";
+            }
+
+            $tanggal = $tahun . "-" . $bulan . "-" . $d;
+            $this->db->select('remarks');
+            $this->db->from('working_calendar');
+            $this->db->where('deleted', 0);
+            $this->db->where('working_date', $tanggal);
+            $data = $this->db->get()->result_array();
+
+            //Jika Hari Minggu
+            $style = "background:white !important;";
+            $checkbox = "<input hidden checked class='checked' type='checkbox' value='" . $d . "' name='days[]' style='float: left; width: 20px;'/>";
+            $note = "<textarea rows='2' name='remarks[]'>" . @$data[0]['remarks'] . "</textarea>";
+
+            if (@$data[0]['remarks'] != "") {
+                $style = "background:#FFDADA !important;";
+            }
+
+            if (date("l", mktime(0, 0, 0, $bulan, $d, $tahun)) == "Sunday") {
+                $style = "background:#FFDADA !important;";
+                $note = "<textarea rows='2' hidden name='remarks[]'></textarea>";
+            }
+
+            $html .= "  <td align=center style='" . $style . "' valign=middle>
+                            $checkbox
+                            <b style='font-size: 20px;'>$d</b><br>
+                            $note
+                        </td>";
+
+            //Jika Sudah seminggu
+            if (date("w", mktime(0, 0, 0, $bulan, $d, $tahun)) == 6) {
+                $html .= "</tr>";
+            }
+        }
+        $html .= '</table>';
+
+        echo $html;
+    }
+
+    public function create()
+    {
+        if ($this->input->post()) {
+            $month = $this->input->post('filter_month');
+            $year = $this->input->post('filter_year');
+            $days = $this->input->post('days');
+            $remarks = $this->input->post('remarks');
+
+            for ($i = 0; $i < count($days); $i++) {
+                $date = $year . "-" . $month . "-" . $days[$i];
+                $remark = @$remarks[$i];
+
+
+                $this->db->select('*');
+                $this->db->from('working_calendar');
+                $this->db->where('deleted', 0);
+                $this->db->where('working_date', $date);
+                $records = $this->db->get()->num_rows();
+
+                if ($remark != "") {
+                    if ($records > 0) {
+                        $this->db->where('working_date', $date);
+                        $this->db->update('working_calendar', ["remarks" => $remark]);
+                    } else {
+                        $send = $this->crud->create('working_calendar', ["working_date" => $date, "remarks" => $remark], "WORK", "WORK");
+                    }
+                } else {
+                    if ($records > 0) {
+                        $this->db->delete('working_calendar', ['working_date' => $date]);
+                    }
+                }
+            }
+
+            echo json_encode(array("title" => "Good Job", "message" => "Data Saved Successfully", "theme" => "success"));
+        } else {
+            show_error("Cannot Process your request");
+        }
+    }
+}
---
+++ application/views/master/working_calendar.php
@@ -0,0 +1,105 @@
+<div id="p" class="easyui-panel" title="Working Calendar" style="width:100%; background: #F4F4F4;">
+    <form id="frm_insert" method="post">
+        <fieldset style="width: 50%; border:2px solid #d0d0d0; margin-bottom: 5px; margin-top: 5px; border-radius:4px; float: left;">
+            <legend><b>Form Filter Data</b></legend>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">Period</span>
+                <select style="width:30%;" name="filter_month" id="filter_month" class="easyui-combobox" data-options="prompt:'Month'">
+                    <option value="01">January</option>
+                    <option value="02">February</option>
+                    <option value="03">March</option>
+                    <option value="04">April</option>
+                    <option value="05">May</option>
+                    <option value="06">June</option>
+                    <option value="07">July</option>
+                    <option value="08">August</option>
+                    <option value="09">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                </select>
+                <input style="width:30%;" name="filter_year" id="filter_year" value="<?= date("Y") ?>" class="easyui-numberbox" data-options="prompt:'Year', validType: 'length[0,4]'">
+            </div>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;"></span>
+                <a href="javascript:;" class="easyui-linkbutton" onclick="filter()"><i class="fa fa-search"></i> Filter Data</a>
+                <a href="javascript:;" class="easyui-linkbutton" onclick="add()"><i class="fa fa-save"></i> Save Data</a>
+            </div>
+        </fieldset>
+        <fieldset style="width: 20%; border:2px solid #d0d0d0; margin-bottom: 5px; margin-top: 5px; border-radius:4px; float: left;">
+            <legend><b>HKW</b></legend>
+            <p style="text-align: center; font-size: 60px !important; margin:0;" id="hkw">0</p>
+        </fieldset>
+        <div id="calendar">
+
+        </div>
+    </form>
+</div>
+<script>
+    //Add Data
+    function add() {
+        var filter_month = $("#filter_month").combobox('getValue');
+        var filter_year = $("#filter_year").textbox('getValue');
+
+        $('#frm_insert').form('submit', {
+            url: '<?= base_url('master/working_calendar/create') ?>',
+            method: 'POST',
+            onSubmit: function() {
+                return $(this).form('validate');
+            },
+            success: function(result) {
+                var result = eval('(' + result + ')');
+                if (result.theme == "success") {
+                    toastr.success(result.message, result.title);
+
+                    calendars(filter_month, filter_year);
+                    hkw(filter_month, filter_year);
+                } else {
+                    toastr.error(result.message, result.title);
+                }
+            }
+        });
+    }
+
+    function filter() {
+        var filter_month = $("#filter_month").combobox('getValue');
+        var filter_year = $("#filter_year").textbox('getValue');
+
+        if (filter_month == "" || filter_year == "") {
+            toastr.warning("Please select Period!", "Information");
+        } else {
+            calendars(filter_month, filter_year);
+            hkw(filter_month, filter_year);
+        }
+    }
+
+    function calendars(month = "", year = "") {
+        $.ajax({
+            type: "post",
+            url: "<?= base_url('master/working_calendar/calendars') ?>",
+            data: "month=" + month + "&year=" + year,
+            dataType: "html",
+            success: function(response) {
+                $("#calendar").html(response);
+            }
+        });
+    }
+
+    function hkw(month = "", year = "") {
+        $.ajax({
+            type: "post",
+            url: "<?= base_url('master/working_calendar/hkw') ?>",
+            data: "month=" + month + "&year=" + year,
+            dataType: "html",
+            success: function(response) {
+                $("#hkw").html(response);
+            }
+        });
+    }
+
+    $(function() {
+        $("#filter_month").combobox('setValue', '<?= date("m") ?>');
+        calendars();
+        hkw();
+    });
+</script>
