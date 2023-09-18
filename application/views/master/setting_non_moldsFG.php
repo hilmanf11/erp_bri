f9fe1ae2c431a0a1aeed449da313dde634902443
kurniawan <kurniawan130795@gmail.com>
2023-09-18 10:48:53 +0700

    setting_non_molds Series

---
+++ application/views/master/setting_non_moldsFG.php
@@ -0,0 +1,349 @@
+<!-- TABLE DATAGRID -->
+<table id="dg" class="easyui-datagrid" style="width:99.5%;" toolbar="#toolbar">
+    <thead>
+        <tr>
+            <th rowspan="2" field="ck" checkbox="true"></th>
+            <th rowspan="2" data-options="field:'item_fg_id',align:'center',width:100">Product ID</th>
+            <th rowspan="2" data-options="field:'item_fg_no',halign:'center',width:150">Product No</th>
+            <th rowspan="2" data-options="field:'item_fg_name',align:'center',width:100">Product Name</th>
+            <th rowspan="2" data-options="field:'machine_id',halign:'center',width:100">Machine ID</th>
+            <th rowspan="2" data-options="field:'machine_no',halign:'center',width:100">Machine No</th>
+            <th rowspan="2" data-options="field:'cycle_time',align:'center',width:100">Cycle Time <br>(shot/second)</th>
+            <th rowspan="2" data-options="field:'priority',align:'center',width:100">Priority</th>
+            <th colspan="2" data-options="field:'',width:100,halign:'center'"> Created</th>
+            <th colspan="2" data-options="field:'',width:100,halign:'center'"> Updated</th>
+            
+        </tr>
+        <tr>
+            <th data-options="field:'created_by',width:100,align:'center'"> By</th>
+            <th data-options="field:'created_date',width:150,align:'center'"> Date</th>
+            <th data-options="field:'updated_by',width:100,align:'center'"> By</th>
+            <th data-options="field:'updated_date',width:150,align:'center'"> Date</th>
+        </tr>
+    </thead>
+</table>
+<!-- TOOLBAR DATAGRID -->
+<div id="toolbar" style="height: 35px;">
+    <?= $button ?>
+</div>
+<!-- DIALOG SAVE AND UPDATE -->
+<div id="dlg_insert" class="easyui-dialog" title="Add New" data-options="closed: true,modal:true" style="width: 400px; padding:10px; top: 20px;">
+    <form id="frm_insert" method="post" novalidate>
+        <fieldset style="width:100%; border:1px solid #d0d0d0; margin-bottom: 10px; border-radius:4px; float: left;">
+            <legend><b>Form Data</b></legend>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">Product No</span>
+                <input style="width:60%;" name="item_fg_id" id="item_fg_id" required="" class="easyui-combogrid">
+            </div>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">Machine No</span>
+                <input style="width:60%;" name="machine_id" id="machine_id" required="" class="easyui-combogrid">
+            </div>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">Cycle Time</span>
+                <input style="width:60%;" name="cycle_time" class="easyui-numberbox">
+            </div>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">Priority</span>
+                <input style="width:60%;" name="priority" class="easyui-numberbox">
+            </div>
+        </div>
+        </fieldset>
+    </form>
+</div>
+<!-- Upload -->
+<div id="dlg_upload" class="easyui-dialog" title="Upload Data" data-options="closed: true,modal:true" style="width: 500px; padding:10px; top: 20px;">
+    <form id="frm_upload" method="post" enctype="multipart/form-data" novalidate>
+        <fieldset style="width:100%; border:1px solid #d0d0d0; margin-bottom: 10px; border-radius:4px; float: left;">
+            <legend><b>Form Data</b></legend>
+            <div class="fitem">
+                <span style="width:35%; display:inline-block;">File Upload</span>
+                <input name="file_upload" style="width: 60%;" required="" accept=".xls" id="file_excel" class="easyui-filebox">
+            </div>
+        </fieldset>
+    </form>
+    <span style="float: left; color:green;">SUCCESS : <b id="p_success">0</b></span><span style="float: right; color:red;"> FAILED : <b id="p_failed">0</b></span>
+    <div id="p_upload" class="easyui-progressbar" style="width:100%; margin-top: 10px;"></div>
+    <center><b id="p_start">0</b> Of <b id="p_finish">0</b></center>
+    <div id="p_remarks" title="History Upload" class="easyui-panel" style="width:100%; height:200px; padding:10px; margin-top: 10px;">
+        <ul id="remarks">
+        </ul>
+    </div>
+</div>
+
+<!-- PDF -->
+<iframe id="printout" src="<?= base_url('master/setting_non_moldsFG/print') ?>" style="width: 100%;" hidden></iframe>
+<script>
+    //ADD DATA
+    function add() {
+        $('#dlg_insert').dialog('open');
+        url_save = '<?= base_url('master/setting_non_moldsFG/create') ?>';
+        $('#frm_insert').form('clear')
+    }
+    //EDIT DATA
+    function update() {
+        var row = $('#dg').datagrid('getSelected');
+        if (row) {
+            $('#dlg_insert').dialog('open');
+            $('#frm_insert').form('load', row);
+            url_save = '<?= base_url('master/setting_non_moldsFG/update') ?>?id=' + btoa(row.id);
+        } else {
+            toastr.warning("Please select one of the data in the table first!", "Information");
+        }
+    }
+    //DELETE DATA
+    function deleted() {
+        var rows = $('#dg').datagrid('getSelections');
+        if (rows.length > 0) {
+            $.messager.confirm('Warning', 'Are you sure you want to delete this data?', function(r) {
+                if (r) {
+                    for (var i = 0; i < rows.length; i++) {
+                        var row = rows[i];
+                        $.ajax({
+                            method: 'post',
+                            url: '<?= base_url('master/setting_non_moldsFG/delete') ?>',
+                            data: {
+                                id: row.id
+                            },
+                            success: function(result) {
+                                var result = eval('(' + result + ')');
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                toastr.error("This item cannot be deleted, Please make sure it didn't have any relation");
+                                // $.messager.alert("Error", jqXHR.statusText, 'error');
+                            },
+                            complete: function(data) {
+                                $('#dg').datagrid('reload');
+                            }
+                        });
+                    }
+                }
+            });
+        } else {
+            toastr.warning("Please select one of the data in the table first!", "Information");
+        }
+    }
+
+    $('#item_fg_id').combogrid({
+            url: '<?= base_url('master/item_fg/reads') ?>',
+            panelWidth: 420,
+            idField: 'id',
+            textField: 'number',
+            mode: 'remote',
+            fitColumns: true,
+            prompt: "Choose Product No",
+            columns: [
+                [{
+                    field: 'number',
+                    title: 'Product No',
+                    width: 100
+                }, {
+                    field: 'name',
+                    title: 'Product Name',
+                    width: 100
+                }, ]
+            ]
+        });
+
+        $('#machine_id').combogrid({
+            url: '<?= base_url('master/machines/reads') ?>',
+            panelWidth: 420,
+            idField: 'id',
+            textField: 'number',
+            mode: 'remote',
+            fitColumns: true,
+            prompt: "Choose Machine No",
+            columns: [
+                [{
+                    field: 'number',
+                    title: 'Machine No',
+                    width: 100
+                },{
+                    field: 'name',
+                    title: 'Machine Name',
+                    width: 100
+                },]
+            ]
+        });
+
+   //Upload Data
+    function upload() {
+        $('#dlg_upload').dialog('open');
+    }
+
+    function download_excel() {
+        window.location.assign('<?= base_url('template/tmp_setting_non_moldsFG.xls') ?>');
+    }
+    //PRINT PDF
+    function pdf() {
+        $("#printout").get(0).contentWindow.print();
+    }
+    //PRINT EXCEL
+    function excel() {
+        window.location.assign('<?= base_url('master/setting_non_moldsFG/print/excel') ?>');
+    }
+    
+    //RELOAD
+    function reload() {
+        window.location.reload();
+    }
+
+    //CELLSTYLE STATUS
+    function cellStyler(value, row, index) {
+        if (value == 0) {
+            return 'background: #53D636; color:white;';
+        } else {
+            return 'background: #FF5F5F; color:white;';
+        }
+    }
+    //FORMATTER STATUS
+    function cellFormatter(value) {
+        if (value == 0) {
+            return 'Active';
+        } else {
+            return 'Inactive';
+        }
+    };
+
+    $(function() {
+        //SETTING DATAGRID EASYUI
+        $('#dg').datagrid({
+            url: '<?= base_url('master/setting_non_moldsFG/datatables') ?>',
+            pagination: true,
+            clientPaging: false,
+            remoteFilter: true,
+            rownumbers: true
+        }).datagrid('enableFilter');
+        //SAVE DATA
+        $('#dlg_insert').dialog({
+            buttons: [{
+                text: 'Save',
+                iconCls: 'icon-ok',
+                handler: function() {
+                    $('#frm_insert').form('submit', {
+                        url: url_save,
+                        onSubmit: function() {
+                            return $(this).form('validate');
+                        },
+                        success: function(result) {
+                            var result = eval('(' + result + ')');
+                            if (result.theme == "success") {
+                                toastr.success(result.message, result.title);
+                            } else {
+                                toastr.error(result.message, result.title);
+                            }
+                            $('#dlg_insert').dialog('close');
+                            $('#dg').datagrid('reload');
+                        }
+                    });
+                }
+            }]
+        });
+
+        //GET CURRENCY
+        $('#currency').combogrid({
+            url: '<?= base_url('master/currencies/reads') ?>',
+            panelWidth: 420,
+            idField: 'name',
+            textField: 'name',
+            mode: 'remote',
+            fitColumns: true,
+            prompt: "Choose Currency",
+            columns: [
+                [{
+                    field: 'symbol',
+                    title: 'Symbol',
+                    width: 100
+                }, {
+                    field: 'number',
+                    title: 'Currency ID',
+                    width: 120
+                }, {
+                    field: 'name',
+                    title: 'Currency Name',
+                    width: 250
+                }, ]
+            ]
+        });
+
+         //Upload Data
+         $('#dlg_upload').dialog({
+            buttons: [{
+                text: 'List Failed',
+                handler: function() {
+                    window.open('<?= base_url('master/setting_non_moldsFG/uploadDownloadFailed') ?>', '_blank');
+                }
+            }, {
+                text: 'Upload',
+                iconCls: 'icon-ok',
+                handler: function() {
+                    $('#frm_upload').form('submit', {
+                        url: '<?= base_url('master/setting_non_moldsFG/upload') ?>',
+                        onSubmit: function() {
+                            if ($(this).form('validate') == false) {
+                                return $(this).form('validate');
+                            } else {
+                                $.messager.progress({
+                                    title: 'Please Wait',
+                                    msg: 'Importing Excel to Database'
+                                });
+                            }
+                        },
+                        success: function(result) {
+                            $.messager.progress('close');
+                            //Clear File
+                            $.ajax({
+                                url: "<?= base_url('master/setting_non_moldsFG/uploadclearFailed') ?>"
+                            });
+                            var json = eval('(' + result + ')');
+                            requestData(json.total, json);
+
+                            function requestData(total, json, number = 1, value = 0, success = 1, failed = 1) {
+                                if (value < 100) {
+                                    value = Math.floor((number / total) * 100);
+                                    $('#p_upload').progressbar('setValue', value);
+                                    $('#p_start').html(number);
+                                    $('#p_finish').html(total);
+
+                                    $.ajax({
+                                        type: "POST",
+                                        async: true,
+                                        url: "<?= base_url('master/setting_non_moldsFG/uploadCreate') ?>",
+                                        data: {
+                                            "data": json[number - 1]
+                                        },
+                                        cache: false,
+                                        dataType: "json",
+                                        success: function(result) {
+                                            if (result.theme == "success") {
+                                                $('#p_success').html(success);
+                                                var title = "<b style='color: green;'>" + result.title + "</b> | " + result.message;
+                                                requestData(total, json, number + 1, value, success + 1, failed + 0);
+                                            } else {
+                                                $('#p_failed').html(failed);
+                                                var title = "<b style='color: red;'>" + result.title + "</b> | " + result.message;
+                                                //Json Failed
+                                                $.ajax({
+                                                    type: "POST",
+                                                    async: true,
+                                                    url: "<?= base_url('master/setting_non_moldsFG/uploadcreateFailed') ?>",
+                                                    data: {
+                                                        data: json[number - 1],
+                                                        message: result.message
+                                                    },
+                                                    cache: false
+                                                });
+                                                requestData(total, json, number + 1, value, success + 0, failed + 1);
+                                            }
+                                            $("#p_remarks").append(title + "<br>");
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    });
+                }
+            }]
+        });
+    });
+</script>
