2c2c564f078178c60582553f291ed7813404560f
arisafriyanto <arisafriyanto1933@gmail.com>
2026-04-30 09:32:12 +0700

    feat: Add Scan In WIP Store functionality with UI and navigation

---
+++ application/controllers/control/Scan_in_wip_store.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set("Asia/Bangkok");
+defined('BASEPATH') or exit('No direct script access allowed');
+class Scan_in_wip_store extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        $this->load->model('crud');
+    }
+
+    public function index()
+    {
+        if (empty($this->session->username)) {
+            redirect('error_session');
+        } elseif ($this->checkuserAccess($this->id_menu()) > 0) {
+            $data['button'] = $this->getbutton($this->id_menu());
+            $this->load->view('template/header', $data);
+            $this->load->view('control/scan_in_wip_store');
+        } else {
+            redirect('error_access');
+        }
+    }
+}
